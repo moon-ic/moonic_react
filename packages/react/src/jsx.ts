76c6b5ddab9ae402d616728beabe74fdb5d61255
moon-ic <3276817499@qq.com>
2024-09-08 20:16:00 +0800

    feat: jsx调试

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -63,4 +63,29 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
 	return ReactElement(type, key, ref, props);
 };
 
-export const jsxDEV = jsx;
+export const jsxDEV = (type: ElementType, config: any) => {
+	const props: Props = {};
+	let key: Key = null;
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = val + '';
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
